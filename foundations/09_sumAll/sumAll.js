e0e528d6b9c8b9d69a19d0b763b0fc510490c8c1
SRPater <srpater@gmail.com>
2026-01-09 12:29:31 +0100

    Finish foundations/09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // Check if both arguments are integers
+    if (!(Number.isInteger(a) && Number.isInteger(b))) {
+        return "ERROR";
+    }
+    
+    // Check if both arguments are positive
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
 
+    // Swap arguments if bigger number comes first
+    if (a > b) {
+        const temp = a;
+        a = b;
+        b = temp;
+    }
+
+    // Sum arguments
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
