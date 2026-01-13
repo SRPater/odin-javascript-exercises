5a2fd933a111b11969160c8d1f2da9510daaace7
SRPater <srpater@gmail.com>
2026-01-13 14:08:28 +0100

    Finish foundations/13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Make lowercase and strip any non alphanumeric character (punctuation and whitespaces)
+    const cleanedString = string.toLowerCase().replace(/[^a-z0-9]/gu, '');
 
+    // Reverse string
+    const reversedString = cleanedString.split("").reverse().join("");
+
+    // Compare cleaned string and reversed string and return result
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
