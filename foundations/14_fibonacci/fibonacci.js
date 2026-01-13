4210b2dd75bf3877f8eed4ad60a7df2c4dada678
SRPater <srpater@gmail.com>
2026-01-13 14:21:16 +0100

    Finished foundations/14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let n;
 
+    if (typeof input !== "number") {
+        n = parseInt(input);
+    } else {
+        n = input;
+    }
+
+    if (n < 0) return "OOPS";
+    
+    const fibArray = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        fibArray[i] = fibArray[i - 2] + fibArray[i - 1];
+    }
+
+    return fibArray[n];
 };
 
 // Do not edit below this line
