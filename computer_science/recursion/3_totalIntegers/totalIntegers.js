ab0640c494ab9fd44f3e00a9dbf687b5c3158f74
SRPater <srpater@gmail.com>
2026-02-09 11:18:47 +0100

    Finish computer_science/recursion/3_totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,23 @@
-const totalIntegers = function() {
-  
+const isObject = (val) => typeof val === "object" && val !== null;
+
+const totalIntegers = function(obj) {
+  let total = 0;
+
+  if (!isObject(obj)) {
+    return;
+  }
+
+  const values = Object.values(obj);
+
+  for (const v of values) {
+    if (Number.isInteger(v)) {
+      total++;
+    } else if (isObject(v)) {
+      total += totalIntegers(v);
+    }
+  }
+
+  return total;
 };
   
 // Do not edit below this line
