0d3ec1ff61c879db07a47e3c7795754301b66d59
SRPater <srpater@gmail.com>
2026-02-09 11:34:58 +0100

    Finish computer_science/recursion/5_pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,15 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+  const result = [1];
+  if (n === 1) return result;
+
+  const prevResult = pascal(n - 1);
+
+  prevResult.forEach((number, index) => {
+    const neighbour = prevResult[index + 1] ?? 0;
+    result.push(number + neighbour);
+  });
+
+  return result;
 };
   
 // Do not edit below this line
